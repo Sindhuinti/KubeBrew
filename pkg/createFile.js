8bfca233f4a5c76e6f5d93df4d6932fd7dd81ab1
Sindhu Inti <iamsindhuinti23@gmail.com>
2023-08-05 17:33:16 +0530

    fix: format code

--- pkg/createFile.js
+++ pkg/createFile.js
@@ -1,21 +1,23 @@
 const fs = require('fs');
 
 const createFile = (inputFile, outputFile) => {
-  fs.readFile(inputFile, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error: gg', err);
-      process.exit(1);
-    } else {
-      fs.writeFile(outputFile, data, 'utf8', (err) => {
+    fs.readFile(inputFile, 'utf8', (err, data) => {
         if (err) {
-          console.error('Error: ', err);
-          process.exit(1);
+            console.error('Error: gg', err);
+            process.exit(1);
         } else {
-          console.log(`File "${outputFile}" created successfully!!!.`);
+            fs.writeFile(outputFile, data, 'utf8', (err) => {
+                if (err) {
+                    console.error('Error: ', err);
+                    process.exit(1);
+                } else {
+                    console.log(
+                        `File "${outputFile}" created successfully!!!.`
+                    );
+                }
+            });
         }
-      });
-    }
-  });
+    });
 };
 
 module.exports = createFile;
--- pkg/docker.js
+++ pkg/docker.js
@@ -3,29 +3,29 @@ const createFile = require('./createFile');
 const path = require('path');
 
 const dockerFiles = new Map([
-  ['Go', 'go-docker.txt'],
-  ['Java', 'java-docker.txt'],
-  ['Node', 'node-docker.txt'],
-  ['Python', 'python-docker.txt'],
+    ['Go', 'go-docker.txt'],
+    ['Java', 'java-docker.txt'],
+    ['Node', 'node-docker.txt'],
+    ['Python', 'python-docker.txt'],
 ]);
 
 async function Docker() {
-  const answers = await inquirer.prompt({
-    name: 'Docker',
-    type: 'list',
-    message: 'Please select the language',
-    choices: ['Go', 'Java', 'Node', 'Python', 'Exit'],
-  });
-  if (answers.Docker == 'Exit') {
-    process.exit(1);
-  }
+    const answers = await inquirer.prompt({
+        name: 'Docker',
+        type: 'list',
+        message: 'Please select the language',
+        choices: ['Go', 'Java', 'Node', 'Python', 'Exit'],
+    });
+    if (answers.Docker == 'Exit') {
+        process.exit(1);
+    }
 
-  const inputFile = path.resolve(
-    __dirname,
-    '../helper/docker/',
-    dockerFiles.get(answers.Docker)
-  );
-  createFile(inputFile, 'DockerFile');
+    const inputFile = path.resolve(
+        __dirname,
+        '../helper/docker/',
+        dockerFiles.get(answers.Docker)
+    );
+    createFile(inputFile, 'DockerFile');
 }
 
 module.exports = Docker;
