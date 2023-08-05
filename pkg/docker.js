9aed4c1cb017d56213465b8eef06e760fa459efb
Sindhu Inti <iamsindhuinti23@gmail.com>
2023-08-05 17:32:24 +0530

    feat: add docker logic

---
+++ pkg/docker.js
@@ -0,0 +1,31 @@
+const inquirer = require('inquirer');
+const createFile = require('./createFile');
+const path = require('path');
+
+const dockerFiles = new Map([
+  ['Go', 'go-docker.txt'],
+  ['Java', 'java-docker.txt'],
+  ['Node', 'node-docker.txt'],
+  ['Python', 'python-docker.txt'],
+]);
+
+async function Docker() {
+  const answers = await inquirer.prompt({
+    name: 'Docker',
+    type: 'list',
+    message: 'Please select the language',
+    choices: ['Go', 'Java', 'Node', 'Python', 'Exit'],
+  });
+  if (answers.Docker == 'Exit') {
+    process.exit(1);
+  }
+
+  const inputFile = path.resolve(
+    __dirname,
+    '../helper/docker/',
+    dockerFiles.get(answers.Docker)
+  );
+  createFile(inputFile, 'DockerFile');
+}
+
+module.exports = Docker;
